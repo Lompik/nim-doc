bdb47b90bbe662cef639955b2c4039222d883361
Lompiktab <stefont@gmail.com>
2016-05-07 20:43:03 +0400

    Move path variables to build.sh

--- build.sh
+++ build.sh
@@ -9,11 +9,23 @@ ref_output_file=nim-ref-$nim_version
 
 makeinfo=makeinfo
 
+# setup for travis
+if [[ ! -z ${TRAVIS+x} ]]
+then
+    travis_build_home=${travis_build_home:-"/home/travis/build/Lompik/nim-doc"}
+    texinfo_bin_path=${texinfo_bin_path:-"$travis_build_home/texinfo-$texinfo_version/tp"}
+    nre_lib_path=${nre_lib_path:-"$travis_build_home/nre/src"}
+    nim_bin_path=${nim_bin_path:-"$travis_build_home/nim-$nim_version/bin"}
+    nim_lib_path=${nim_lib_path:-"$travis_build_home/nim-$nim_version/lib"}
+    makeinfo=${makeinfo:-$texinfo_bin_path/texi2any} # old makeinfos output errors
+    echo 'path:"$nre_lib_path/nre/src"' > nim.cfg # workaround for -p with abs path
+fi
 
-if [ ! type nim &> /dev/null ]
+if [ ! type nim &> /dev/null ] # outside of travis, allow customization
 then
     export PATH=$PATH:"$nim_bin_path"
 fi
+
 
 if [[ -z ${nim_version+x} ]]
 then # try to some nim config
@@ -23,14 +35,6 @@ fi
 if [[ -z ${nim_lib_path+x} ]]
 then
     nim_lib_path=$(grep 'lib$' <(nim dump 2>&1))
-fi
-
-
-
-if [[ ! -z ${TRAVIS+x} ]]
-then
-    makeinfo=$texinfo_bin_path/texi2any # old makeinfos output errors
-    echo 'path:"/home/travis/nre/src"' > nim.cfg # workaround for -p with abs path
 fi
 
 ulimit -n 3000
@@ -57,7 +61,7 @@ fi
 echo "Building the texi/info docs"
 
 cd "$ref_output_dir"
-"$start_dir"/nim_texi $nim_lib_path > $ref_output_file.texi
+"$start_dir"/nim_texi $nim_lib_path | tee $ref_output_file.texi
 $makeinfo --no-split $ref_output_file.texi
 gzip $ref_output_file.info
 cd "$start_dir"
