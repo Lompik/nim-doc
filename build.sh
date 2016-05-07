ab2fbeff09bc8864ca2b1f79c071f274a641e497
Lompiktab <stefont@gmail.com>
2016-05-07 18:35:06 +0400

    Initial commit

---
+++ build.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+set -e
+
+start_dir="$PWD"
+
+ref_output_dir="$PWD/doc"
+ref_output_file=nim-ref-$nim_version
+
+makeinfo=makeinfo
+
+
+if [ ! type nim &> /dev/null ]
+then
+    export PATH=$PATH:"$nim_bin_path"
+fi
+
+if [[ -z ${nim_version+x} ]]
+then # try to some nim config
+    nim_version=$(grep 'Version [0-9\.]*' <(nim --version 2>&1) | grep -o '[0-9]*\.[0-9]*\.[0-9]*')
+fi
+
+if [[ -z ${nim_lib_path+x} ]]
+then
+    nim_lib_path=$(grep 'lib$' <(nim dump 2>&1))
+fi
+
+
+
+if [[ ! -z ${TRAVIS+x} ]]
+then
+    $makeinfo=$texinfo_bin_path/texi2any # old makeinfos output errors
+    echo 'path:"/home/travis/nre/src"' > nim.cfg # workaround for -p with abs path
+fi
+
+ulimit -n 3000
+
+echo "building json -> texi converter"
+nim c nim_texi.nim
+
+set +e
+echo "Extracting json doc from .nim's"
+for file in $(find $nim_lib_path -iname '*.nim')
+do
+    nim jsondoc $file &> /dev/null
+done
+set -e
+
+if [[ -e "$ref_output_dir/$ref_output_file.info" ]]
+then
+    rm "$ref_output_dir/$ref_output_file*"
+elif [[ ! -d "$ref_output_dir" ]]
+then
+    mkdir "$ref_output_dir" ;
+fi
+
+echo "Building the texi/info docs"
+
+cd "$ref_output_dir"
+"$start_dir"/nim_texi $nim_lib_path > $ref_output_file.texi
+$makeinfo --no-split $ref_output_file.texi
+gzip $ref_output_file.info
+cd "$start_dir"
+
+echo DONE.
---
+++ nim_texi.nim
@@ -0,0 +1,121 @@
+import json
+import strutils
+import os
+import osproc
+import nre
+import tables
+
+type symbol_type = enum
+  skConst=0, skIterator, skLet, skProc, skTemplate, skType, skVar, skMacro, skConverter
+var symbols, symbols_empty = {
+  "skConst": (skConst,"@vindex","","Constant variables"),
+  "skIterator": (skIterator,"@findex","","Iterators"),
+  "skLet": (skLet,"@vindex","","Let variable"),
+  "skProc": (skProc,"@findex","","Procedures"),
+  "skTemplate": (skTemplate,"@findex","","Templates"),
+  "skType": (skType,"@vindex","","Types"),
+  "skMacro": (skMacro,"@findex","","Macros"),
+  "skConverter": (skConverter,"@findex","","Converter"),
+  "skMethod": (skConverter,"@findex","","Methods"),
+  "skVar": (skVar,"@vindex","","Variables")}.totable
+
+proc texize(txt:string):string=
+  txt.replace("@", "@@").replace("}","@}").replace("{","@{")
+
+
+var  tmpname = "/tmp/516516156161"
+proc parse_symb_json(n_json:JsonNode, module:string)=
+  symbols=symbols_empty
+  for i in countUp(0,n_json.len- 1):
+    var output="\n\n"
+    var stype = n_json[i]["type"].str
+    output &= """
+@item $1
+  """ % [n_json[i]["name"].str.texize]
+    output &= "$1 $2\n" % [(symbols[stype])[1],n_json[i]["code"].str.texize, module]
+
+    if(n_json[i].haskey("code")):
+      output &= """
+@example
+$1
+@end example
+  """ % n_json[i]["code"].str.texize
+
+
+    if(n_json[i].haskey("description")):
+      writeFile(tmpname,n_json[i]["description"].str)
+      output &= execProcess( @["/usr/bin/pandoc","-f","html","-t","texinfo", tmpname].join(" "),
+                        @[] ).replace("@node Top","").replace("@top Top","").replace(re"@ref{.*?,(.*?)}","\1")
+      tmpname.removeFile()
+      discard system.open(tmpname, fmWrite)
+
+    #let id = (symbols[stype])[0]
+    symbols[stype][2] = symbols[stype][2] & output
+
+proc Main()=
+  var chapters = ""
+  var modules : seq[string] = @[]
+  var modulei : seq[string] = @[]
+  var json_dir = commandLineParams()[0]
+  for file in walkDirRec(json_dir ):
+    var (mpath, module, ext) = splitFile(file)
+    if ext != ".json":
+      continue
+    var n_json = json.parseFile(file)
+    var mod1 = mpath.split(json_dir)
+    module = mod1[mod1.len - 1] & "/" & module
+    modulei.add(module)
+    modules.add("* " & module & "::\n")
+    chapters &= "\n@node $1\n" % module
+    parse_symb_json(n_json, module)
+    for stype in symbols.keys:
+      if symbols[stype][2] != "":
+         symbols[stype][2] ="""
+@chapter $1
+
+@itemize
+$2
+@end itemize
+""" % [symbols[stype][3], symbols[stype][2]]
+
+    chapters &= @[symbols["skConst"][2], symbols["skLet"][2], symbols["skVar"][2], symbols["skType"][2],   symbols["skProc"][2], symbols["skTemplate"][2],symbols["skMacro"][2],symbols["skConverter"][2],symbols["skMethod"][2],symbols["skIterator"][2]].join("\n")
+
+  echo """@settitle The Nim Manual
+@ifnottex
+@node Top
+@top Nim info Manual
+@end ifnottex
+
+@menu
+"""
+  echo modules.join("")
+  echo """
+
+* Command and Function Index:: Functions
+* Variable Index:: variables
+@end menu
+
+hello!
+"""
+  echo chapters
+
+  echo """
+@node Command and Function Index
+@unnumbered Command and function index
+
+@printindex fn
+
+@node Variable Index
+@unnumbered Variable index
+
+This is not a complete index of variables and faces, only the ones that are
+mentioned in the manual.  For a more complete list, use @kbd{M-x
+org-customize @key{RET}} and then click yourself through the tree.
+
+@printindex vr
+
+"""
+Main()
+# Local Variables:
+# firestarter: "nim c -d:release nim_texi.nim || notify-send -u low 'nim' 'compile error on nim_texi.nim'"
+# End:
