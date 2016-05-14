7704f52afc3ee30b0c97f7bc7fadb3eb4fe1c78c
Lompiktab <stefont@gmail.com>
2016-05-14 12:04:38 +0400

    use nre from stdlib with LD_PRELOAD. Get rid of texinfo

--- build.sh
+++ build.sh
@@ -11,12 +11,10 @@ ref_output_file=nim-ref-$nim_version
 if [[ ! -z ${TRAVIS+x} ]]
 then
     travis_build_home=${travis_build_home:-"/home/travis/build/Lompik/nim-doc"}
-    texinfo_bin_path=${texinfo_bin_path:-"$travis_build_home/texinfo-$texinfo_version/tp"}
-    nre_lib_path=${nre_lib_path:-"$travis_build_home/nre/src"}
+    pcre_lib_path=${nre_lib_path:-"$travis_build_home/pcre/lib"}
     nim_bin_path=${nim_bin_path:-"$travis_build_home/nim-$nim_version/bin"}
     nim_lib_path=${nim_lib_path:-"$travis_build_home/nim-$nim_version/lib"}
     echo "path:\"$nre_lib_path\"" > nim.cfg # workaround for -p with abs path
-    echo "path:\""$(readlink -f "$nim_lib_path/../compiler")"\"" >> nim.cfg
     echo "path:\""$(readlink -f "$nim_lib_path/..")"\"" >> nim.cfg
     echo "path:\"$nim_lib_path/packages/docutils\"" >> nim.cfg
     cat nim.cfg # debug
@@ -66,7 +64,12 @@ fi
 echo "Building the texi/info docs"
 
 cd "$ref_output_dir"
-"$start_dir"/nim_texi $nim_lib_path > $ref_output_file.texi
+if [[ ! -z ${TRAVIS+x} ]]
+then ## cant compile with dynoverride yet https://github.com/nim-lang/Nim/issues/3646
+    LD_LIBRARY_PATH=$pcre_lib_path "$start_dir"/nim_texi $nim_lib_path > $ref_output_file.texi
+else
+    "$start_dir"/nim_texi $nim_lib_path > $ref_output_file.texi
+fi
 makeinfo --no-split $ref_output_file.texi &> /dev/null
 gzip $ref_output_file.info
 cd "$start_dir"
